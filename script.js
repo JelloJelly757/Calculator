59d77c620324eab16b7af8da749a23b8e0ca1308
Elizabeth <elizabethg2796@tuckahoeschools.org>
2025-02-27 13:29:46 +0000

    intial commit

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const btnZero = document.getElementById('btnZero');
+const btnOne = document.getElementById('btnOne');
+const btnTwo = document.getElementById('btnTwo');
+const btnThree = document.getElementById('btnThree');
+const btnFour = document.getElementById('btnFour');
+const btnFive = document.getElementById('btnFive');
+const btnSix = document.getElementById('btnSix');
+const btnSeven = document.getElementById('btnSeven');
+const btnEight = document.getElementById('btnEight');
+const btnNine = document.getElementById('btnNine');
+const btnAdd = document.getElementById('btnAdd');
+const btnSub = document.getElementById('btnSub');
+const btnMulti = document.getElementById('btnMulti');
+const btnDiv = document.getElementById('btnDiv');
 const screen = document.getElementById('screen');
+
 
 btnZero.addEventListener("click", button0);
 
@@ -7,3 +21,95 @@ function button0() {
   let text = "0";
   document.getElementById('screen').innerHTML = text;
 }
+
+btnOne.addEventListener("click", button1);
+
+function button1() {
+  let text = "1";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnTwo.addEventListener("click", button2);
+
+function button2() {
+  let text = "2";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnThree.addEventListener("click", button3);
+
+function button3() {
+  let text = "3";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnFour.addEventListener("click", button4);
+
+function button4() {
+  let text = "4";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnFive.addEventListener("click", button5);
+
+function button5() {
+  let text = "5";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnSix.addEventListener("click", button6);
+
+function button6() {
+  let text = "6";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnSeven.addEventListener("click", button7);
+
+function button7() {
+  let text = "7";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnEight.addEventListener("click", button8);
+
+function button8() {
+  let text = "8";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnNine.addEventListener("click", button9);
+
+function button9() {
+  let text = "9";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnNine.addEventListener("click", button9);
+
+function button9() {
+  let text = "9";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnNine.addEventListener("click", button9);
+
+function button9() {
+  let text = "9";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnNine.addEventListener("click", button9);
+
+function button9() {
+  let text = "9";
+  document.getElementById('screen').innerHTML = text;
+}
+
+btnNine.addEventListener("click", button9);
+
+function button9() {
+  let text = "9";
+  document.getElementById('screen').innerHTML = text;
+}
+
